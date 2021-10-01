a17b416d0de57ce0d92e1d5c9d9a31a46a18a461
David Burton <david.burton@revature.net>
2021-10-01 14:31:51 -0500

    Notification tests still aren't working

--- src/test/java/com/revature/ocean/controllers/NotificationControllerTest.java
+++ src/test/java/com/revature/ocean/controllers/NotificationControllerTest.java
@@ -1,10 +1,12 @@
 package com.revature.ocean.controllers;
 
+import com.revature.ocean.services.NotificationService;
 import com.revature.ocean.services.UserService;
 import com.revature.ocean.utility.JwtUtility;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.mockito.Mockito;
@@ -19,31 +21,50 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class NotificationControllerTest {
-/*David: ... So. NotificationController.
-
-AFAICT it can't be instantiated. Which means it can't be tested.*/
-
     /*
-    NotificationController notificationController;
+        @Mock
     UserService userService;
+    @Mock
+    EmailService emailService;
+    @Mock
+    HttpSession session;
+    @Mock
     JwtUtility jwtUtility;
+
+    @BeforeEach
+    void setUp() {
+        this.userController = new UserController(userService, emailService, jwtUtility);
+        User user = new User("Shane", "Password");
+        userController.createUser(user);
+    }
+    * */
+/*
+    NotificationController notificationController;
+
+    @Mock
+    UserService userService;
+    @Mock
+    JwtUtility jwtUtility;
+    @Mock
+    NotificationService notificationService;
+
     Map<String, String> headers = new HashMap<>();
 
     @BeforeEach
     public void setup(){
 
-        jwtUtility = mock(JwtUtility.class);
+/*        jwtUtility = mock(JwtUtility.class);
         userService = mock(UserService.class);
+        notificationService = mock(NotificationService.class);*/
+/*
+        this.notificationController = new NotificationController(notificationService, userService, jwtUtility);
 
-
-        //notificationController = new NotificationController(userService, jwtUtility);
-
-        //headers.put("authorization", "testing");
+        headers.put("authorization", "testing");
     }
 
     @Test
     public void getAll(){
-        notificationController.getAllNotification(1,headers);
+        this.notificationController.getAllNotification(1,headers);
     }
 
     @Test
@@ -51,6 +72,6 @@ AFAICT it can't be instantiated. Which means it can't be tested.*/
         //when(headers.get("authorization")).thenReturn(null);
         headers.put("authorization", null);
 
-        assertNull(notificationController.getAllNotification(1,headers));
-    }
-*/}
+        assertNull(this.notificationController.getAllNotification(1,headers));
+    }*/
+}
