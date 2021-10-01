ffbced79a913ed50f0e0cb3842fbc1e6f96b238d
Tommy Arnette <tommy.arnette@gmail.com>
2021-10-01 11:28:45 -0400

    Added welcomeEmail test to EmailServiceTest class

--- src/test/java/com/revature/ocean/services/EmailServiceTest.java
+++ src/test/java/com/revature/ocean/services/EmailServiceTest.java
@@ -85,4 +85,57 @@ class EmailServiceTest {
 
         assertEquals(expectedMessageContents, actualMessageContents);
     }
+
+    @Test
+    void welcomeEmail(){
+        EmailService emailService = Mockito.spy(this.emailService);
+
+        SimpleMailMessage welcome = new SimpleMailMessage();
+        welcome.setFrom("oceansocailapp@gmail.com");
+
+        User user = new User("user", "password", "tommy.arnette@gmail.com", "Tommy", "TEST", new Date());
+
+        welcome.setTo(user.getEmail());
+        welcome.setText("Hello "+ user.getFirstName() +",\n" +
+                "This email is to confirm your account has been added\n" +
+                " to the Ocean Social Application.\n" +
+                "\n"+
+                "\n"+
+                "To get started please update your profile and then\n" +
+                "get to posting in your 'Tide'.\n"+
+                "\n"+
+                "\n"+
+                "Check out other peoples' post and view their profile.\n"+
+                "\n"+
+                "Leave comments and likes on your friends' post to show them some love. \n"+
+                "\n"+
+                "\n"+
+                "If you have any issues please feel free to reach out to our administrators.\n" +
+                "at oceansocailapp@gmail.com\n" +
+                "\n"+
+                "Thank you and have a great day!");
+
+        String expectedWelcomeEmail = welcome.getText();
+
+        String actualWelcomeEmail = "Hello "+ "Tommy" +",\n" +
+                "This email is to confirm your account has been added\n" +
+                " to the Ocean Social Application.\n" +
+                "\n"+
+                "\n"+
+                "To get started please update your profile and then\n" +
+                "get to posting in your 'Tide'.\n"+
+                "\n"+
+                "\n"+
+                "Check out other peoples' post and view their profile.\n"+
+                "\n"+
+                "Leave comments and likes on your friends' post to show them some love. \n"+
+                "\n"+
+                "\n"+
+                "If you have any issues please feel free to reach out to our administrators.\n" +
+                "at oceansocailapp@gmail.com\n" +
+                "\n"+
+                "Thank you and have a great day!";
+
+        assertEquals(expectedWelcomeEmail, actualWelcomeEmail);
+    }
 }
